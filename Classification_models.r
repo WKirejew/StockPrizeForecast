8032c22ad3d397272e1d5826fcecf334d610eb67
Wojciech Kirejew <wojtasss99@gmail.com>
2021-12-08 16:13:22 +0100

    Dzień1

--- Classification_models.r
+++ Classification_models.r
@@ -1,12 +1,12 @@
 ##Porównanie wyników klasyfikacji trendów EURUSD dla różnych rodzajów modeli machine learningu i ich łączenia # nolint
-#1. Akwizycja danych
-#2. Obróbka danych
-#3. Rozszerzenie ilości wymiarów poprzez uwzględnienie czynników analizy ekonomicznej 
-#4. Analiza statystyczna i redukcja wymiarowości
-#5. Dobór parametrów dla kolejnych typów modeli wraz z dyskusją uzyskiwanych wyników: walidacja over- lub under- fittingu
-#6. Analiza porównawcza uzyskanych wyników
-#7. Łączenie modeli
-#8. Porównanie wyników
+1. Akwizycja danych
+2. Obróbka danych
+3. Rozszerzenie ilości wymiarów poprzez uwzględnienie czynników analizy ekonomicznej 
+4. Analiza statystyczna i redukcja wymiarowości
+5. Dobór parametrów dla kolejnych typów modeli wraz z dyskusją uzyskiwanych wyników: walidacja over- lub under- fittingu
+6. Analiza porównawcza uzyskanych wyników
+7. Łączenie modeli
+8. Porównanie wyników
 ##Porównanie wyników dla algorytmów ML wykorzystanych do przewidywania zmiany trendu
 
-read.csv("\\EURUSD.csv")
+token: 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
