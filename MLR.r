62e878ae186aa74698b412b81cf4468643bb5970
Kirejew Wojciech (STUD) <01122464@pw.edu.pl>
2021-12-09 00:19:19 +0100

    Day1

--- MLR.r
+++ MLR.r
@@ -1,9 +1,20 @@
 library(readr)
 library(tidyverse)
+library(TTR)
+
+## 1. Pobranie danych i wstępna obróbka
 eurusd <- "https://raw.github.com/WKirejew/ProjektML/master/EURUSD.csv"
 adata <- read_csv(eurusd)
 adata <- subset(adata, Volume > 0)
 adata <- mutate(adata, Date = substring(adata$Time[], 1, 10))
 adata$Time[] <- substring(adata$Time[], 12, 13)
 adata = select(adata, Date, Time, Close, High, Low, Volume)
-print(adata[1,])
+
+## 2. Obliczenie i dodanie wskaźników analizy technicznej
+#Lista parametrów:
+rsin <- 14
+adata <- mutate(adata, RSI = RSI(adata$Close, n = rsin))
+bdata <- adata[-c(1:rsin),]
+
+
+print(bdata[1,])
