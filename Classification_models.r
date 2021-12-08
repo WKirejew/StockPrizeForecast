1307e8d35056ed73d14a17b5ef702c32a881bb69
Wojciech Kirejew <wojtasss99@gmail.com>
2021-12-08 13:12:06 +0100

    Początek projektu i test git

---
+++ Classification_models.r
@@ -0,0 +1,12 @@
+##Porównanie wyników klasyfikacji trendów EURUSD dla różnych rodzajów modeli machine learningu i ich łączenia # nolint
+#1. Akwizycja danych
+#2. Obróbka danych
+#3. Rozszerzenie ilości wymiarów poprzez uwzględnienie czynników analizy ekonomicznej 
+#4. Analiza statystyczna i redukcja wymiarowości
+#5. Dobór parametrów dla kolejnych typów modeli wraz z dyskusją uzyskiwanych wyników: walidacja over- lub under- fittingu
+#6. Analiza porównawcza uzyskanych wyników
+#7. Łączenie modeli
+#8. Porównanie wyników
+##Porównanie wyników dla algorytmów ML wykorzystanych do przewidywania zmiany trendu
+
+read.csv("\\EURUSD.csv")
